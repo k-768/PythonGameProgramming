c6128887a4f6d304db76ebf0a4736cbfec267e97
k768 <R20034@st.omu.ac.jp>
2024-10-01 13:54:55 +0900

    update memo.txt, pandoc-make.yaml, move.py and test.py

--- samples/move.py
+++ samples/move.py
@@ -1,20 +1,51 @@
 import tkinter as tk
-import os
 
-# ディレクトリ
-cwd = os.getcwd()
-
-# ウィンドウ設置
+# ウィンドウ設置　#? Tkinterの基礎(これで学ぶ)
 root = tk.Tk()
 root.title("tkinter test")
 root.geometry("600x300")
 
 # キャンバス設置
-canvas = tk.Canvas(root,width = 200,height = 100,bg = "skyblue")
+canvas_x = 600 #? 変数 ... OK
+canvas_y = 300
+canvas = tk.Canvas(root,width = canvas_x,height = canvas_y,bg = "yellowGreen")
 canvas.pack()
 
 # 四角形を配置
-canvas.create_rectangle(10,10,60,60,fill="blue",tag="rect")
+x = 100
+y = 100
+
+size_x = 50
+size_y = 50
+canvas.create_rectangle(x,y,x+size_x,y+size_y,fill="blue",tag="rect") #? 四則演算 ... OK(変数回)
+
+
+def press(e): #? 関数の定義と実行
+    global x,y,size_x,size_y
+    key = e.keysym
+    dx = 0
+    dy = 0
+    if(key == "w" or key == "Up"): #? 条件分岐，論理演算子
+        dx = 0
+        dy = -10
+    elif(key == "a"  or key == "Left"):
+        dx = -10
+        dy = 0
+    elif(key == "s" or key == "Down"):
+        dx = 0
+        dy = 10
+    elif(key == "d" or key == "Right"):
+        dx = 10
+        dy = 0
+    
+    if(0 <= x+dx <= canvas_x - size_x and 0 <= y+dy  <= canvas_y - size_y ):
+        x = x + dx
+        y = y + dy
+        canvas.delete("rect")
+        canvas.create_rectangle(x,y,x+size_x,y+size_y,fill="blue",tag="rect")
+
+#キー入力をトリガーに関数を呼び出すよう設定する
+root.bind("<KeyPress>", press)#? キー入力監視(これで学ぶ)
 
 # メインループ
 root.mainloop()
--- samples/test.py
+++ samples/test.py
@@ -1,8 +1,4 @@
-import random
+print(1)
 
-#サイコロ
-print(random.randint(1,50)*2)
-
-fishes = ["アジ","サバ","イワシ","タイ","サメ"]
-fish_weights =[30,30,30,8,2] #アジ,サバ,イワシ30%、タイ8%、サメ2%
-print(random.choices(fishes,k=1,weights = fish_weights)[0])
+pri
+nt(2)
